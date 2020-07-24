bdde059973fbc35058630b438df92e8b6d46b94e
dhruvtyagi <dhruvtyagi4@gmail.com>
2020-07-24 11:07:52 +0530

    Update article.ctrl.js

--- server/controllers/article.ctrl.js
+++ server/controllers/article.ctrl.js
@@ -47,5 +47,34 @@ module.exports = {
             next()            
         })
     },
-    
+    clapArticle: (req, res, next) => {
+        Article.findById(req.body.article_id).then((article)=> {
+            return article.clap().then(()=>{
+                return res.json({msg: "Done"})
+            })
+        }).catch(next)
+    },
+    commentArticle: (req, res, next) => {
+        Article.findById(req.body.article_id).then((article)=> {
+            return article.comment({
+                author: req.body.author_id,
+                text: req.body.comment
+            }).then(() => {
+                return res.json({msg: "Done"})
+            })
+        }).catch(next)
+    },
+    getArticle: (req, res, next) => {
+        Article.findById(req.params.id)
+        .populate('author')
+        .populate('comments.author').exec((err, article)=> {
+            if (err)
+                res.send(err)
+            else if (!article)
+                res.send(404)
+            else
+                res.send(article)
+            next()            
+        })
+    }
 }
